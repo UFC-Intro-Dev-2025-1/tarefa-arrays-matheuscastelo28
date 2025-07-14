7a34d093856249d79f09b851131fdff7933d3f85
matheus castelo <castelomatheus9@gmail.com>
2025-07-14 15:38:43 +0000

    atv feita

--- src/main.ts
+++ src/main.ts
@@ -16,29 +16,34 @@
 // 10. Calcular a soma de todos os elementos do array usando o método reduce. ❌
 
 const numeros: number[] = [10, 20, 30, 40, 50];
-
-function mostrarArray(array: number[]): void {
-    console.log('--------------------------------');
-    console.log(`Tamanho: `);
-    console.log(`Array: `);
-}
+ function mostrarArray(array: number []) : void {
+    let tamanhoDoArray = array.length;
+    console.log('-------------------------------------------------');
+    console.log(`{Tamanho:${tamanhoDoArray} `);
+    console.log(`Array:${array} `);
+ }
+ numeros.push(60,70);
 mostrarArray(numeros);
 
+let ultimo = numeros.pop();
 
-
-
-
-const elementoExcluido = ;
+const elementoExcluido = ultimo;
 mostrarArray(numeros);
-console.log(`Elemento excluído: ${elementoExcluido}`);
+console.log(`Elemento excluido : ${elementoExcluido}`);
 
-numeros.forEach();
+numeros.forEach(function( item) {
+    console.log(item);
+});
 
-const dobrados = numeros.map();
+const dobrados = numeros.map((x: number )=> x *2);
+
 mostrarArray(dobrados);
 
-const soma = numeros.reduce();
-console.log(`Soma dos elementos do array: ${soma}`);
+const soma = numeros.reduce((acumulador, valorAtual )=> acumulador + valorAtual, 0);
+    console.log(`Soma  dos elementos do array: ${soma} `);
+
+
+
 
 // para rodar o código, use o comando:
 // npm run build
